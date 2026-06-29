381debb1158042a524b22119f374d33e765bbe66
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-06-29 12:07:49 +0500

    import and use excalifont

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+// use our exaclifont!
+tailwind.config = {
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Excalifont", "sans-serif"]
+            }
+        }
+    }
+}
+
+
 console.log("Yup, its loaded");
