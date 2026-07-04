585827c219cb06070f977242be91654e48014ff7
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-07-05 00:13:16 +0500

    add github and api to frontend

--- script.js
+++ script.js
@@ -11,4 +11,16 @@ tailwind.config = {
 
 // umm code starts here??
 
+async function onloaded() {
+    fetch("https://api.alimad.co/github/info").then(r => r.json()).then(d => onGithub(d));
+}
+
+async function onGithub(data) {
+    // data recieved, saturate
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    onloaded();
+})
+
 console.log("Yup, its loaded");
