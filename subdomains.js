a71fc18afbe2c84035b29d07117ce7a45153fbd9
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-07-15 16:56:56 +0500

    add more projects and dates

--- subdomains.js
+++ subdomains.js
@@ -1,235 +1,76 @@
-export default function subdomains() {
-    return {
-        count: {
-            path: "/",
-            desc: "CountMad",
-            subtitle: "Based on SpacetimeDB",
-            repo: "https://github.com/Alimadcorp/Count"
-        },
-        falooda: {
-            path: "/",
-            desc: "Falooda Fanatics",
-            subtitle: "Falooda Fanatics is a Hack Club Club in Faislabad",
-            repo: "https://github.com/Alimadcorp/falooda"
-        },
-        meet: {
-            path: "/",
-            desc: "Schedule a meeting w/ me",
-            subtitle: "hehehe"
-        },
-        study: {
-            path: "/",
-            desc: "Study Resources",
-            subtitle: "A bunch of stuff i wanted to share w/ fellow students"
-        },
-        "312": {
-            path: "/",
-            desc: "3121534312",
-            subtitle: "If u know u know",
-            repo: "https://github.com/Alimadcorp/312"
-        },
-        ws: {
-            path: "/",
-            desc: "WebSocket",
-            subtitle: "Used in ChatMad and for LogWays",
-            repo: "https://github.com/Alimadcorp/websocket"
-        },
-        up: {
-            path: "/",
-            desc: "F1nn CDN",
-            subtitle: "Cloudflare R2 storage"
-        },
-        qr: {
-            path: "/",
-            desc: "QR code generator",
-            subtitle: "Create QR codes FAST",
-            repo: "https://github.com/Alimadcorp/qr"
-        },
-        besideu: {
-            path: "/",
-            desc: "This one's long gone 😭",
-            subtitle: "A location aware social app",
-            repo: "https://github.com/Alimadcorp/BesideU"
-        },
-        mc: {
-            path: "/",
-            desc: "Our minecraft server 😋",
-            subtitle: "This one's ded"
-        },
-        blog: {
-            path: "/",
-            desc: "THE blog",
-            subtitle: "hehe",
-            repo: "https://github.com/Alimadcorp/MyWebsite/tree/main/extras/blog"
-        },
-        games: {
-            path: "/",
-            desc: "Fun lil signup experience",
-            subtitle: "Submission to r/BadUIBattles",
-            repo: "https://github.com/Alimadcorp/HtmlGames"
-        },
-        cdn: {
-            path: "/",
-            desc: "Alimad CDN",
-            subtitle: "Another Cloudflare bucket"
-        },
-        static: {
-            path: "/Ricky.mp4",
-            desc: "Quick access to /f/static/** of cdn",
-            subtitle: "Just a redirect"
-        },
-        status: {
-            path: "/",
-            desc: "Uptime and status",
-            subtitle: "Check the health of my services (Doesnt go down much but yea)"
-        },
-        emoji: {
-            path: "/",
-            desc: "Emoji CDN",
-            subtitle: "All of hackclub's emoji in ONE PLACE",
-            repo: "https://github.com/Alimadcorp/emojicdn"
-        },
-        life: {
-            path: "/",
-            desc: "Conway's Game of Life",
-            subtitle: "In one line of HTML",
-            repo: "https://github.com/Alimadcorp/life/blob/main/index.html"
-        },
-        campfire: {
-            path: "/",
-            desc: "Campfire dashboard",
-            subtitle: "Depracated, you will get redirected to campfire.hackclub.com instead",
-            repo: "https://github.com/Alimadcorp/Campfire"
-        },
-        mail: {
-            path: "/",
-            desc: "Alimad Mailing services",
-            subtitle: "I use this to get read reciepts for physical mail P:"
-        },
-        hackclub: {
-            path: "/",
-            desc: "Hack Club slack statistics",
-            subtitle: "Now at stats.hackclub.community too!",
-            repo: "https://github.com/Alimadcorp/HackClubStats"
-        },
-        "api.hackclub": {
-            path: "/api/stats",
-            desc: "Hack Club Stats API",
-            subtitle: "yup"
-        },
-        live: {
-            path: "/",
-            desc: "How many ppl are viewing dis page rn?",
-            subtitle: "Demo at top right corner of the screen btw",
-            repo: "https://github.com/Alimadcorp/Live"
-        },
-        webtools: {
-            path: "/",
-            desc: "Unity Web build tools",
-            subtitle: "Useful utilities for unity devs",
-            repo: "https://github.com/Alimadcorp/WebBuildTools"
-        },
-        somps: {
-            path: "/",
-            desc: "SOMPS dashboard",
-            subtitle: "Made for the summer of making",
-            repo: "https://github.com/Alimadcorp/SOMPS"
-        },
-        wordle: {
-            path: "/",
-            desc: "Today's wordle answer",
-            subtitle: "Also the history of it",
-            repo: "https://github.com/Alimadcorp/MyWebsite/tree/main/extras/wordle"
-        },
-        server: {
-            path: "/",
-            desc: "Ts is secret :P",
-            subtitle: "."
-        },
-        "gcu.societies": {
-            path: "/",
-            desc: "GCU Societies",
-            subtitle: "Dead app I made for my college"
-        },
-        "stats.github": {
-            path: "/",
-            desc: "GitHub statistics",
-            subtitle: "Clone of Github Readme Statistics",
-            repo: "https://github.com/Alimadcorp"
-        },
-        chat: {
-            path: "/?channel=global",
-            desc: "ChatMad",
+// nvm this file is cooked
+
+chat: {
+    path: "/?channel=global",
+        desc: "ChatMad",
             subtitle: "Anonymous? Chatting",
-            repo: "https://github.com/Alimadcorp/chat"
-        },
-        proxy: {
-            path: "/api/proxy?url=https://jsonplaceholder.typicode.com/todos/1",
-            desc: "API Proxy",
+                repo: "https://github.com/Alimadcorp/chat"
+},
+proxy: {
+    path: "/api/proxy?url=https://jsonplaceholder.typicode.com/todos/1",
+        desc: "API Proxy",
             subtitle: "Bypass CORS for testing and stuff"
-        },
-        log: {
-            path: "/api/log?channel=main",
-            desc: "Yum",
+},
+log: {
+    path: "/api/log?channel=main",
+        desc: "Yum",
             subtitle: "Logs whatever you send P:"
-        },
-        replace: {
-            path: "/",
-            desc: "MegaReplace",
+},
+replace: {
+    path: "/",
+        desc: "MegaReplace",
             subtitle: "Tired of CTRL+F replace?"
-        },
-        extra: {
-            path: "/wmj24tier",
-            desc: "Smoll extra stuff",
+},
+extra: {
+    path: "/wmj24tier",
+        desc: "Smoll extra stuff",
             subtitle: "hehe",
-            repo: "https://github.com/Alimadcorp/extras"
-        },
-        "itch.xport": {
-            path: "/",
-            desc: "Itch.io data export",
+                repo: "https://github.com/Alimadcorp/extras"
+},
+"itch.xport": {
+    path: "/",
+        desc: "Itch.io data export",
             subtitle: "Export a list of ur games",
-            repo: "https://github.com/Alimadcorp/itch.xport"
-        },
-        watch: {
-            path: "/",
-            desc: "StupidWatch",
+                repo: "https://github.com/Alimadcorp/itch.xport"
+},
+watch: {
+    path: "/",
+        desc: "StupidWatch",
             subtitle: "State-of-the-art stopwatch GWAWHAHWHAHW",
-            repo: "https://github.com/Alimadcorp/stupidwatch"
-        },
-        meme: {
-            path: "/",
-            desc: "Meme of the day",
+                repo: "https://github.com/Alimadcorp/stupidwatch"
+},
+meme: {
+    path: "/",
+        desc: "Meme of the day",
             subtitle: "yup",
-            repo: "https://github.com/Alimadcorp/meme"
-        },
-        jam: {
-            path: "/search",
-            desc: "Jam result",
+                repo: "https://github.com/Alimadcorp/meme"
+},
+jam: {
+    path: "/search",
+        desc: "Jam result",
             subtitle: "An itch.io embeddable",
-            repo: "https://github.com/Alimadcorp/jamresult"
-        },
-        quantarax: {
-            path: "/",
-            desc: "QuantaraX",
+                repo: "https://github.com/Alimadcorp/jamresult"
+},
+quantarax: {
+    path: "/",
+        desc: "QuantaraX",
             subtitle: "A website i made for some uhhh guy",
-            repo: "https://github.com/Alimadcorp/QuantaraX"
-        },
-        sendup: {
-            path: "/",
-            desc: "Sendup Schedule generator",
+                repo: "https://github.com/Alimadcorp/QuantaraX"
+},
+sendup: {
+    path: "/",
+        desc: "Sendup Schedule generator",
             subtitle: "Made for GCU students",
-            repo: "https://github.com/Alimadcorp/sendup"
-        },
-        theme: {
-            path: "/",
-            desc: "Theme generator for game jams",
+                repo: "https://github.com/Alimadcorp/sendup"
+},
+theme: {
+    path: "/",
+        desc: "Theme generator for game jams",
             subtitle: "Yup",
-            repo: "https://github.com/Alimadcorp/Themegen"
-        },
-        http: {
-            path: "/",
-            desc: "Quick HTTP Client",
+                repo: "https://github.com/Alimadcorp/Themegen"
+},
+http: {
+    path: "/",
+        desc: "Quick HTTP Client",
             subtitle: "Supports CORS bypass"
-        },
-    };
 }
