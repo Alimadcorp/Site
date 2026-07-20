539f4aa2028a0a7d4e2c4a7a8abc6833411c4710
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-07-20 15:40:39 +0500

    add leaderboard rank finder

--- api/index.js
+++ api/index.js
@@ -64,15 +64,78 @@ app.get('/auth/hackatime/callback', async (req, res) => {
         });
 
         const username = userResponse.data?.github_username; //  yup we got the username
-        return res.redirect(`${hackabeat_url}/#access_token=${access_token}&username=${username}&key=${k}`);
+        return res.redirect(`${hackabeat_url}/#access_token=${access_token}&username=${username}&uid=${userResponse.data?.id}&key=${k}`);
     } catch (error) {
         console.error('OAuth Exchange Failed:', error.response?.data || error.message);
         return res.status(500).send('Authentication routine failed.');
     }
 });
 
+app.get("/beat/leaderboard", async (req, res) => {
+    const t = process.env.HC_COOKIE;
+    let uid = req.query.uid;
+    if (!uid) return res.sendStatus(400);
+    uid = parseInt(uid);
+    if (!t) return res.sendStatus(503);
+
+    const i = await fetch("https://hackatime.hackclub.com/leaderboards?period_type=daily&scope=global", {
+        headers: { "Cookie": t }
+    });
+
+    if (!i.ok) {
+        console.error(i.status);
+        return res.sendStatus(500);
+    }
+
+    const html = await i.text();
+    const m = html.match(/(?<=\<script data\-page\="app" type\="application\/json">).*?(?=\<\/script\>)/s);
+    const match = m ? m[0] : null;
+
+    if (!match) { return res.sendStatus(500); }
+
+    const ver = JSON.parse(match).version;
+    const r = await fetch("https://hackatime.hackclub.com/leaderboards?period_type=daily&scope=global",
+        {
+            headers: {
+                "Cookie": t,
+                "X-Inertia": true,
+                "X-Inertia-Partial-Component": "Leaderboards/Index",
+                'X-Inertia-Partial-Data': "entries",
+                "X-Inertia-Version": ver
+            }
+        }
+    );
+
+    const w = await r.json();
+    const data = w.props?.entries?.entries;
+    if (!(data && data.length > 0)) return res.sendStatus(500);
+
+    // data is in format:
+    /* 
+        [
+            {
+                user_id: Int,
+                total_seconds: Int,
+                streak_count: Int,
+                is_current_user: Bool,
+                user: Object { display_name, avatar_url, profile_path, verified: Bool, country_code, red: Bool },
+                active_project: Object { name, repo_url }
+            }
+        ]
+    */
+
+    let global_rank, local_rank, country, response;
+    const me = data.find(e => e.user_id === uid);
+    country = me.user.country_code;
+
+    global_rank = data.findIndex(e => e.user_id === uid) + 1;
+    local_rank = data.filter(e => e.user.country_code == country).findIndex(e => e.user_id === uid) + 1;
+
+    return res.json({ name: me.user.display_name, pfp: me.user.avatar_url, link: "https://hackatime.hackclub.com" + me.user.profile_path, local_rank, global_rank });
+});
+
 app.listen(port, () => {
     console.log("Meow");
 });
 
-// ts so empty for now hehe
+// aint so empty no more
