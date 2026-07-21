09b90a33cbd6b16974006f0b89b2d8542aa7e554
Alimadcorp <119115454+Alimadcorp@users.noreply.github.com>
2026-07-22 04:51:14 +0500

    add write date

--- sitemap.js
+++ sitemap.js
@@ -5,7 +5,7 @@ const path = require('path');
 
 const domain = 'https://alimad.co';
 
-const exclude = "api,CNAME,history.txt,README.md,robots.txt,givaway.txt,script.js,generate.js,sitemap.js,sitemap.xml,meow.js,style.css,todo.txt,generateOG.js,package.json,node_modules,package-lock.json,example-blog.html,projects.txt".split(",");
+const exclude = "api,CNAME,history.txt,README.md,robots.txt,tailwind.config.js,givaway.txt,script.js,generate.js,sitemap.js,sitemap.xml,meow.js,style.css,todo.txt,generateOG.js,package.json,node_modules,package-lock.json,example-blog.html,projects.txt".split(",");
 
 let urls = [];
 
